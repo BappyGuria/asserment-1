a7e3fb97166c0092b3d47907bc187f0e7da22b23
BappyGuria <69080967+BappyGuria@users.noreply.github.com>
2024-03-15 21:47:23 +0600

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'class.dart';
+
+void main(){
+  //Creating instance of Media
+  Media mediaInst = Media();
+
+  Song songInst = Song("Bappy Guria");
+
+  //Calling the play
+  mediaInst.play();
+  songInst.play();
+}
